df82e31627178636eb60e9e874feb76feaf14aa8
prateeksingh <prateeksingh450@gmail.com>
2023-11-11 19:42:22 +0530

    Flask API

--- api/app.py
+++ api/app.py
@@ -1,5 +1,6 @@
 from flask import Flask,request
-
+from joblib import  load
+import numpy as np
 app = Flask(__name__)
 
 @app.route("/")
@@ -16,11 +17,34 @@ def sum_two_numbers(x,y):
     res = int(x) + int(y)
     return f"sum of {x} and {y} is {res}"
 
+# model = load()
+# @app.route("/predict", methods = ['POST'])
+# def predict_fn():
+#     input_data = request.get_json()
+#     x = input_data['x']
+#     y = input_data['y']
+#     # res = int(x) + int(y)
+#     return f"sum of {x} and {y} is {x + y}"
 
 @app.route("/predict", methods = ['POST'])
 def predict_fn():
     input_data = request.get_json()
-    x = input_data['x']
-    y = input_data['y']
-    # res = int(x) + int(y)
-    return f"sum of {x} and {y} is {x + y}"
+    img1 = input_data['img1']
+    img2 = input_data['img2']
+
+    img1 = list(map(float, img1))
+    img2 = list(map(float, img2))
+
+    img1 = np.array(img1).reshape(1,-1)
+    img2 = np.array(img2).reshape(1,-1)
+
+    model = load("/Users/prateeksingh/digits-classification/models/production_model.joblib")
+    predicted1 = model.predict(img1)
+    predicted2 = model.predict(img2)
+    if predicted1[0] == predicted2[0]:
+        print("Both images are of same digit")
+        return "True"
+    else:
+        print("Both images are not of same digit")
+        return "False"
+    
