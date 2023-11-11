2804c11b5a8bad7f0234fe047cae7c4702c371c3
prateeksingh <prateeksingh450@gmail.com>
2023-11-11 19:55:57 +0530

    flask api via docker

--- api/app.py
+++ api/app.py
@@ -17,7 +17,15 @@ def sum_two_numbers(x,y):
     res = int(x) + int(y)
     return f"sum of {x} and {y} is {res}"
 
-#image compare
+# model = load()
+# @app.route("/predict", methods = ['POST'])
+# def predict_fn():
+#     input_data = request.get_json()
+#     x = input_data['x']
+#     y = input_data['y']
+#     # res = int(x) + int(y)
+#     return f"sum of {x} and {y} is {x + y}"
+
 @app.route("/predict", methods = ['POST'])
 def predict_fn():
     input_data = request.get_json()
@@ -30,12 +38,13 @@ def predict_fn():
     img1 = np.array(img1).reshape(1,-1)
     img2 = np.array(img2).reshape(1,-1)
 
-    model = load("models/production_model.joblib")
+    model = load("/Users/prateeksingh/digits-classification/models/production_model.joblib")
     predicted1 = model.predict(img1)
     predicted2 = model.predict(img2)
     if predicted1[0] == predicted2[0]:
+        print("Both images are of same digit")
         return "True"
     else:
+        print("Both images are not of same digit")
         return "False"
     
-    # return f"{predicted1}, {predicted2}"
